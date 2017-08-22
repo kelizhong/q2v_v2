9d24c74000306bbd29f51a04245397705011fad3
Zhong, keli <keliz@amazon.com>
2017-08-22 08:11:41 +0800

    refractor

--- inference.py
+++ inference.py
@@ -28,7 +28,7 @@ from utils.file_util import ensure_dir_exists
 class Inference(object):
     """inference model to encode the sequence to vector"""
 
-    def __init__(self, tf_config=None, batch_size=65536, min_words=-1, max_sequences=sys.maxsize):
+    def __init__(self, tf_config=None, min_words=-1, max_sequences=sys.maxsize):
         """
         Parameters
         ----------
@@ -44,7 +44,7 @@ class Inference(object):
                                           unk_token=config('_unk_', section='vocabulary_symbol'))
         self.batch_data = BatchDataTrigramHandler(tokens_fn=self.tokens_helper.tokens,
                                                   batch_size=sys.maxsize, min_words=min_words, enable_target=False)
-        self.batch_size = batch_size
+        self.batch_size = self.tf_config['batch_size']
         self.max_sequences = max_sequences
 
     def _init_model(self):
@@ -79,65 +79,56 @@ class Inference(object):
         proj_name: project name
 
         """
-        model_path = os.path.join(config('project_dir'), 'data', 'visualize')
-        writer = tf.summary.FileWriter(model_path, self.sess.graph)
+        visualization_dir = config('visualization_dir')
+        writer = tf.summary.FileWriter(visualization_dir, self.sess.graph)
         proj_config = projector.ProjectorConfig()
         embed = proj_config.embeddings.add()
         embed.tensor_name = tensor_name
-        meta_path = os.path.join(model_path, "%s.tsv" % proj_name)
+        meta_path = os.path.join(visualization_dir, "%s.tsv" % proj_name)
         embed.metadata_path = meta_path
         projector.visualize_embeddings(writer, proj_config)
-        sources = set(map(str.strip, open(file)))
-        metadata_path = embed.metadata_path
 
-        sources_list = []
-        embedding_list = []
-        h5f = h5py.File('%s.h5' % proj_name, 'w')
-        # the left over elements that would be truncated by zip
-        with tqdm(total=len(sources)) as pbar:
-            for count, each in enumerate(zip(*[iter(sources)] * self.batch_size)):
-                batch_sources, result = self.encode(each)
-                if result is not None:
-                    sources_list.extend(batch_sources)
-                    embedding_list.append(result)
-                pbar.update(self.batch_size)
-                if count * self.batch_size > self.max_sequences:
-                    print("Have reached to the max sequence %d" % self.max_sequences)
-                    break
-
-        with open(metadata_path, 'w+') as item_file:
-            item_file.write('id\tchar\n')
-            for i, each in enumerate(sources_list):
-                item_file.write('{}\t{}\n'.format(i, each))
-            print('metadata file created')
-
-        concat = np.concatenate(embedding_list, axis=0)
-        h5f.create_dataset(proj_name, data=concat)
+        concat = self.vectorization(file, meta_path=embed.metadata_path)
         item_size, unit_size = concat.shape
         item_embedding = tf.get_variable(embed.tensor_name, [item_size, unit_size])
         assign_op = item_embedding.assign(concat)
         self.sess.run(assign_op)
         saver = tf.train.Saver([item_embedding])
-        saver.save(self.sess, os.path.join(model_path, "%s.embs" % proj_name))
+        saver.save(self.sess, os.path.join(visualization_dir, "%s.embs" % proj_name))
 
-    def vectorization(self, file, proj_name="q2v"):
+    def vectorize(self, file, proj_name="q2v"):
         """
         encode the sequences in file to vector and store in model to visualize them in tensorboard
         Parameters
         ----------
         file: file contains sequences
-        tensor_name: embedding tensorf name
         proj_name: project name
+
+        """
+
+        vector_dir = config('vectorization_dir')
+        ensure_dir_exists(vector_dir)
+        h5_path = os.path.join(vector_dir, "%s.h5" % proj_name)
+        h5f = h5py.File(h5_path, 'w')
+
+        meta_path = os.path.join(vector_dir, "%s.tsv" % proj_name)
+
+        concat = self.vectorization(file, meta_path=meta_path)
+        h5f.create_dataset(proj_name, data=concat)
+
+    def vectorization(self, file, meta_path):
+        """
+        encode the sequences in file to vector and store in model to visualize them in tensorboard
+        Parameters
+        ----------
+        file: file contains sequences
+        meta_path: meta file path
 
         """
         sources = set(map(str.strip, open(file)))
 
         sources_list = []
         embedding_list = []
-        vector_path = os.path.join(config('project_dir'), 'data', 'vectorization')
-        ensure_dir_exists(vector_path)
-        h5_path = os.path.join(vector_path, "%s.h5" % proj_name)
-        h5f = h5py.File(h5_path, 'w')
         # the left over elements that would be truncated by zip
         with tqdm(total=len(sources)) as pbar:
             for count, each in enumerate(zip(*[iter(sources)] * self.batch_size)):
@@ -150,14 +141,14 @@ class Inference(object):
                     print("Have reached to the max sequence %d" % self.max_sequences)
                     break
 
-        meta_path = os.path.join(vector_path, "%s.tsv" % proj_name)
+        ensure_dir_exists(meta_path)
         with open(meta_path, 'w+') as item_file:
             item_file.write('id\tchar\n')
             for i, each in enumerate(sources_list):
                 item_file.write('{}\t{}\n'.format(i, each))
             print('metadata file created')
-        concat = np.concatenate(embedding_list, axis=0)
-        h5f.create_dataset(proj_name, data=concat)
+        vector_concat = np.concatenate(embedding_list, axis=0)
+        return vector_concat
 
     @property
     @memoized
@@ -193,14 +184,15 @@ def setup_logger():
 
 
 def main(_):
+    """For test"""
     setup_logger()
     os.environ['CUDA_VISIBLE_DEVICES'] = "0"
     tf_config = OrderedDict(sorted(FLAGS.__flags.items()))
-    i = Inference(batch_size=1024, tf_config=tf_config)
+    i = Inference(tf_config=tf_config)
     # i.visualize('./data/rawdata/query_sample_inference')
     # i.nearest("women grey nike shoes", './data/rawdata/query_inference', 10)
     # i.nearest("Microsoft Windows 10 Home USB Flash Drive", './data/rawdata/query_inference', 50)
-    i.vectorization('./data/rawdata/query_inference')
+    i.vectorize(tf_config['query_file'])
 
 
 if __name__ == "__main__":
